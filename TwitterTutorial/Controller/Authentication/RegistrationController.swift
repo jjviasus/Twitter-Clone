fd8f8902da24fde4850a9906996c2b74774e095e
jjviasus <61569318+jjviasus@users.noreply.github.com>
2022-05-08 23:09:57 -0400

    Upload user data to firebase

--- TwitterTutorial/Controller/Authentication/RegistrationController.swift
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class RegistrationController: UIViewController {
     
     // MARK: - Properties
     
     private let imagePicker = UIImagePickerController()
+    private var profileImage: UIImage? // optional because it will start off as nil
     
     private let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -25,7 +27,7 @@ class RegistrationController: UIViewController {
         let view = Utilities().inputContainerView(withImage: #imageLiteral(resourceName: "mail"), textField: emailTextField)
         return view
     }()
-
+    
     private lazy var passwordContainerView: UIView = {
         let view = Utilities().inputContainerView(withImage: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
         return view
@@ -35,7 +37,7 @@ class RegistrationController: UIViewController {
         let view = Utilities().inputContainerView(withImage: #imageLiteral(resourceName: "ic_person_outline_white_2x"), textField: fullnameTextField)
         return view
     }()
-
+    
     private lazy var usernameContainerView: UIView = {
         let view = Utilities().inputContainerView(withImage: #imageLiteral(resourceName: "ic_person_outline_white_2x"), textField: usernameTextField)
         return view
@@ -94,7 +96,48 @@ class RegistrationController: UIViewController {
     }
     
     @objc func handleRegistration() {
-        print("Handle Registration")
+        guard let profileImage = self.profileImage else {
+            print("DEBUG: Please select a profile image..")
+            return
+        }
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
+        
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        // A unique identifier for the profile image file
+        let filename = NSUUID().uuidString
+        let storageReference = STORAGE_PROFILE_IMAGES.child(filename)
+        
+        // Puts the file in the firebase storage
+        storageReference.putData(imageData, metadata: nil) { meta, error in
+            storageReference.downloadURL { url, error in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                // Create the user in the firebase authentication
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error is \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    // A unique identifier for the user
+                    guard let uid = result?.user.uid else { return }
+                    
+                    let values = ["email": email,
+                                  "username": username,
+                                  "fullname": fullname,
+                                  "profileImageUrl": profileImageUrl]
+                    
+                    // Update the valuesi in the firebase database
+                    let userReference = REF_USERS.child(uid)
+                    userReference.updateChildValues(values) { error, ref in
+                        print("DEBUG: Successfully updated user information..")
+                    }
+                }
+            }
+        }
     }
     
     @objc func handleShowLogin() {
@@ -117,16 +160,16 @@ class RegistrationController: UIViewController {
         stack.axis = .vertical
         stack.spacing = 20
         stack.distribution = .fillEqually
-
+        
         view.addSubview(stack)
         stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
         
         view.addSubview(alreadyHaveAccountButton)
         alreadyHaveAccountButton.anchor(left: view.leftAnchor,
-                                     bottom: view.safeAreaLayoutGuide.bottomAnchor,
-                                     right: view.rightAnchor,
-                                     paddingLeft: 40,
-                                     paddingRight: 40)
+                                        bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                        right: view.rightAnchor,
+                                        paddingLeft: 40,
+                                        paddingRight: 40)
     }
 }
 
@@ -137,9 +180,10 @@ extension RegistrationController: UIImagePickerControllerDelegate, UINavigationC
     // When image has been selected and choose button is clicked
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let profileImage = info[.editedImage] as? UIImage else { return }
+        self.profileImage = profileImage
         
         plusPhotoButton.layer.cornerRadius = 128 / 2         // Round the photo
-
+        
         plusPhotoButton.layer.masksToBounds = true
         plusPhotoButton.imageView?.contentMode = .scaleAspectFill // scales the image to fit the frame that it is contained within
         plusPhotoButton.imageView?.clipsToBounds = true // makes sure the image doesn't go outside of the bounds of the frame
---
+++ TwitterTutorial/Utils/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  TwitterTutorial
+//
+//  Created by Justin Viasus on 5/8/22.
+//
+
+import Firebase
+import FirebaseStorage
+
+// Global constants (accessible anywhere in the app)
+
+// Firebase: Storage
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+// Firebase: Realtime Database
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
+
